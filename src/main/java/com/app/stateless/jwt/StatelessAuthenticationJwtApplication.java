52c31d1a6d330257b8a7638c1920274307141db0
pratikw008 <pratikwankhede008@gmail.con>
2022-01-13 13:38:53 +0530

    First Commit

---
+++ src/main/java/com/app/stateless/jwt/StatelessAuthenticationJwtApplication.java
@@ -0,0 +1,13 @@
+package com.app.stateless.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatelessAuthenticationJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatelessAuthenticationJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/stateless/jwt/StatelessAuthenticationJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.stateless.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatelessAuthenticationJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
